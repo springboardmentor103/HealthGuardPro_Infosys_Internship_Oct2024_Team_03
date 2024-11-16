a4a0bee2ce12e76f9d91186fa9c03b28981dbca8
rniranjana992 <rniranjana992@gmail.com>
2024-11-16 11:29:21 +0530

    removed the part which was not required

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import SignupPage from './pages/signup';
 
+
 function App() {
   return (
     <div className="App">
      <SignupPage />
+    
     </div>
   );
 }
