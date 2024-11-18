a10f1acedf177f648cdf4dd2a3e1828566c21df0
rniranjana992 <rniranjana992@gmail.com>
2024-11-18 15:54:26 +0530

    modified app.js code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,3 @@
-import React from 'react';
-import Login from './pages/login';
-
-function App() {
-    return (
-        <div>
-            <Login />
-        </div>
-    );
-}
 import './App.css';
 import SignupPage from './pages/signup'
 
