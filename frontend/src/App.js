e69f8a8545200435f6ede9ad702c482161b8adb6
rniranjana992 <rniranjana992@gmail.com>
2024-11-18 20:26:09 +0530

    modified app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
+import logo from './logo.svg';
 import './App.css';
-import SignupPage from './pages/signup'
-
-const App = () => {
+import SignupPage from './pages/signup';
+import HealthAssessmentForm from './pages/HealthAssessmentForm';
+function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <SignupPage />
+      <HealthAssessmentForm />
     </div>
   );
-};
-
+}
 export default App;
