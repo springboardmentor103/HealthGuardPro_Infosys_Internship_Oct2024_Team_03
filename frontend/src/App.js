e84295fa94f85910ff6da1903992a8a034f3e154
vrushali bhandare <vrushalib2304@gmail.com>
2024-11-18 12:55:48 +0530

    add routing from login page to forgotpassword page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import Login from './pages/login';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/login'; // Correct import for Login
+import ForgotPassword from './pages/forgotpassword'; // Correct import for ForgotPassword
 
 function App() {
-    return (
-        <div>
-            <Login />
-        </div>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
