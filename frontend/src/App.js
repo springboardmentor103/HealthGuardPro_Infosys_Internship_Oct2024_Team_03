6833cb8fd1fffdb6e5b9b868fbac593c60c724cc
rniranjana992 <rniranjana992@gmail.com>
2024-11-18 20:31:42 +0530

    modified app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import SignupPage from './pages/signup';
-import HealthAssessmentForm from './pages/HealthAssessmentForm';
 function App() {
   return (
     <div className='App'>
       <SignupPage />
-      <HealthAssessmentForm />
     </div>
   );
 }
