556ffdf66ac82da3e031ea7dee95317dfe59c7d2
rniranjana992 <rniranjana992@gmail.com>
2024-11-20 21:28:18 +0530

    made the signup page

---
+++ frontend/src/pages/signup.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import SignupPage from './pages/signup';
+
+
+function App() {
+  return (
+    <div className="App">
+     <SignupPage />
+    
+    </div>
+  );
+};
+
+export default App;
